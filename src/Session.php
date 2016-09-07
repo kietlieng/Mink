ba19271d4ee616fd60a758739886ff0663e9f1b7
kietlieng <klieng@illuminateed.net>
2016-09-06 19:08:58 -0700

    fixing namespace

--- src/Session.php
+++ src/Session.php
@@ -8,7 +8,7 @@
  * file that was distributed with this source code.
  */
 
-namespace behat\Mink;
+namespace Behat\Mink;
 
 use Behat\Mink\Driver\DriverInterface;
 use Behat\Mink\Selector\SelectorsHandler;
